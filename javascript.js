b29e09415643f75fc2d82e4e615ab8eb9f1a1cde
Edwin E <edwin.emilien92@gmail.com>
2023-02-10 17:18:57 -0500

    Add operate function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// Basic arithmetic functions
 function add(a = 0, b = 0){
     return a + b; 
 }
@@ -12,4 +13,24 @@ function multiply(a, b){
 
 function divide(a, b){
     return a / b;
+}
+
+function operate(operator, number1, number2){
+    let a = number1;
+    let b = number2;
+
+        if(operator == "add"){
+            let calculate = add(a,b);
+                return calculate;
+        } else if(operator == "subtract"){
+            let calculate = subtract(a,b);
+                return calculate;
+        } else if(operator == "multiply"){
+            let calculate = multiply(a,b);
+                return calculate;
+        } else if(operator == "divide"){
+            let calculate = divide(a,b);
+                return calculate;
+        }
+
 }
