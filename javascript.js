0d1359f1c73c6486f6712b36bdbe95d737c41442
Edwin E <edwin.emilien92@gmail.com>
2023-02-10 16:27:45 -0500

    Add 4 basic operation in function

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function add(a = 0, b = 0){
+    return a + b; 
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
